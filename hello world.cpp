f8fbc5265d7846ebef974ab01849fad563afb4d8
zixooo <zixooo@mail.ru>
2022-04-01 20:03:34 +0300

    pizza

--- hello world.cpp
+++ hello world.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int main(){
-    string name;
-    cin >> name;
-    cout << "Hello World! from @" << name  << endl;
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello World! from @" << name  << std::endl;
     return 0;
 
 }
